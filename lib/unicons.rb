0883abf62f0ce375799d9bc49d3739440d98c9d5
Ben Ehmke <benehmke@gmail.com>
2019-01-15 12:00:35 -0600

    update asset path

--- lib/unicons.rb
+++ lib/unicons.rb
@@ -1,7 +1,7 @@
 class Unicons
   
   def self.import
-    json ||= JSON.parse(File.read(File.expand_path("../../app/assets/fonts/unicons.json", __FILE__)))
+    json ||= JSON.parse(File.read(File.expand_path("../../vendor/assets/fonts/unicons.json", __FILE__)))
   end
 
   def self.parse
@@ -29,8 +29,8 @@ class Unicons
   def self.base64
     data ||= import
     @@base64 ||= {
-      :ttf => Base64.encode64(File.read(File.expand_path("../../app/assets/fonts/#{data['filename']}.ttf", __FILE__))).gsub(/\n/, ""),
-      :woff => Base64.encode64(File.read(File.expand_path("../../app/assets/fonts/#{data['filename']}.woff", __FILE__))).gsub(/\n/, "")
+      :ttf => Base64.encode64(File.read(File.expand_path("../../vendor/assets/fonts/#{data['filename']}.ttf", __FILE__))).gsub(/\n/, ""),
+      :woff => Base64.encode64(File.read(File.expand_path("../../vendor/assets/fonts/#{data['filename']}.woff", __FILE__))).gsub(/\n/, "")
     }
   end
 
@@ -50,12 +50,12 @@ if defined?(::Rails)
   end
 elsif defined?(::Jekyll::Assets)
   asset_dirs = ['fonts', 'stylesheets', 'images', 'javascripts'].map do |ad|
-    File.join(File.dirname(__FILE__), '..', 'app', 'assets', ad)
+    File.join(File.dirname(__FILE__), '..', 'vendor', 'assets', ad)
   end
   asset_dirs.select!{ |dir| Dir.exists?(dir) }
   Jekyll::Assets::HOOKS << Proc.new { |env| asset_dirs.each { |dir| env.append_path(dir) } }
 else
   class Unicons
-    ASSET_PATH = File.expand_path("../app/assets", __dir__)
+    ASSET_PATH = File.expand_path("../vendor/assets", __dir__)
   end
 end
